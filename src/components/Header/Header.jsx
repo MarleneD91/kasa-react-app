29d031ad46db9b911e4e28fccc143627cb1ff963
Marlène D <marlene.a.diaz@gmail.com>
2023-05-25 09:19:44 +0200

    Changin header logo href to work w/ isActive condition

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -14,7 +14,7 @@ import '../../sass/components/header.scss'
 const Header = () => {
     return (
         <div className="header">
-            <a href="/home"><img className="pink-logo" src={PinkLogo} alt="kasa pink logo"></img></a>
+            <a href="/"><img className="pink-logo" src={PinkLogo} alt="kasa pink logo"></img></a>
             <Navbar/>
         </div>
     )
